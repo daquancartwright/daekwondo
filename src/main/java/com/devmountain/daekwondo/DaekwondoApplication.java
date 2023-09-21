1a85f09a3201a7b0f6576fc1283d46c66807d8b9
DaQuan Cartwright <daquancartwright@outlook.com>
2023-09-20 21:07:20 -0700

    initial commit

---
+++ src/main/java/com/devmountain/daekwondo/DaekwondoApplication.java
@@ -0,0 +1,13 @@
+package com.devmountain.daekwondo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaekwondoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaekwondoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmountain/daekwondo/DaekwondoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmountain.daekwondo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaekwondoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
